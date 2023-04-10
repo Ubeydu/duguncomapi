45d5a21af209926c769436c51309eb77e3d07c1f
Ubeydu <u.keles@gmail.com>
2023-04-10 15:30:11 +0300

    Modified models.py. Set 'auto_now' to True for date fields in my Product class data model.

--- products/models.py
+++ products/models.py
@@ -11,8 +11,8 @@ class Product(models.Model):
     isim = models.CharField(max_length=200)
     fiyat = models.DecimalField(max_digits=10, decimal_places=2)
     durum = models.CharField(max_length=10, choices= AVAILABLE_STATUS, default='Aktif')
-    olusturulma_tarihi = models.DateTimeField()
-    guncelleme_tarihi = models.DateTimeField()
+    olusturulma_tarihi = models.DateTimeField(auto_now=True)
+    guncelleme_tarihi = models.DateTimeField(auto_now=True)
     
     
 
