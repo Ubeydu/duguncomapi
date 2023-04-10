ef1efc842c4f8cc9bd8c5e214005a7fce07b4847
Ubeydu <u.keles@gmail.com>
2023-04-10 15:09:40 +0300

    Forgot to 'migrate' the data model changes. I did that and now the data model is written into the db.sqlite3.

--- products/models.py
+++ products/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
+AVAILABLE_STATUS = (
+    ("Aktif","Aktif"),
+    ("Pasif", "Pasif"),
+    ("Bilinmiyor","Bilinmiyor"),
+)
+
+
 class Product(models.Model):
     isim = models.CharField(max_length=200)
     fiyat = models.DecimalField(max_digits=10, decimal_places=2)
-    durum = models.CharField(max_length=10, options=AVAILABLE_STATUS, default='active')
+    durum = models.CharField(max_length=10, choices= AVAILABLE_STATUS, default='Aktif')
     olusturulma_tarihi = models.DateTimeField()
     guncelleme_tarihi = models.DateTimeField()
     
-
-
-AVAILABLE_STATUS = (
-    ('active','AKTIF'),
-    ('passive', 'PASIF'),
-    ('other','BILINMIYOR')
-)
+    
 
