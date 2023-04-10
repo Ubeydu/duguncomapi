89a1f6b6659df6d69c90c8042070d687b7f91b6e
Ubeydu <u.keles@gmail.com>
2023-04-10 14:48:00 +0300

    Created models.py, where I created a class called Product. This is Django's way of creating a data object.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    isim = models.CharField(max_length=200)
+    fiyat = models.DecimalField(max_digits=10, decimal_places=2)
+    durum = models.CharField(max_length=10, options=AVAILABLE_STATUS, default='active')
+    olusturulma_tarihi = models.DateTimeField()
+    guncelleme_tarihi = models.DateTimeField()
+    
+
+
+AVAILABLE_STATUS = (
+    ('active','AKTIF'),
+    ('passive', 'PASIF'),
+    ('other','BILINMIYOR')
+)
+
