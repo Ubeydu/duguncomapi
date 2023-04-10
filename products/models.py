66d5244e6d1759e3d321fdcd39a7d478a5bd48ad
Ubeydu <u.keles@gmail.com>
2023-04-10 15:49:18 +0300

    Added MoneyField to have a currency data field to use for fiyat variable.

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 AVAILABLE_STATUS = (
     ("Aktif","Aktif"),
@@ -9,10 +10,12 @@ AVAILABLE_STATUS = (
 
 class Product(models.Model):
     isim = models.CharField(max_length=200)
-    fiyat = models.DecimalField(max_digits=10, decimal_places=2)
+    #fiyat = models.DecimalField(max_digits=10, decimal_places=2)
     durum = models.CharField(max_length=10, choices= AVAILABLE_STATUS, default='Aktif')
     olusturulma_tarihi = models.DateTimeField(auto_now=True)
     guncelleme_tarihi = models.DateTimeField(auto_now=True)
+    fiyat = MoneyField(max_digits=10, decimal_places=2, default_currency='TRY')
+
     
     
 
