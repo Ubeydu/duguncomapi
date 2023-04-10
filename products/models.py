3a5dbcdfedb58efe5c2026ecdbfec47b29e4fab5
Ubeydu <u.keles@gmail.com>
2023-04-10 17:00:26 +0300

    Changed 'olusturulma_tarihi' data field's argument from auto_now to auto_now_add. That way, this date will be populated once when we create a product. The other field 'guncelleme_tarihi' stays the same. 'auto_now' does what we want. It registers the time whenever there is some changes on the object.

--- products/models.py
+++ products/models.py
@@ -10,9 +10,8 @@ AVAILABLE_STATUS = (
 
 class Product(models.Model):
     isim = models.CharField(max_length=200)
-    #fiyat = models.DecimalField(max_digits=10, decimal_places=2)
     durum = models.CharField(max_length=10, choices= AVAILABLE_STATUS, default='Aktif')
-    olusturulma_tarihi = models.DateTimeField(auto_now=True)
+    olusturulma_tarihi = models.DateTimeField(auto_now_add=True)
     guncelleme_tarihi = models.DateTimeField(auto_now=True)
     fiyat = MoneyField(max_digits=10, decimal_places=2, default_currency='TRY')
 
